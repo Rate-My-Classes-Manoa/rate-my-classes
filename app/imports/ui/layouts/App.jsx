84e441b75d0b7918657a3eb12b0b705df31a73ce
Mr. Young <masyoung@hawaii.edu>
2021-04-19 13:28:43 -1000

    Worked on the Careers page

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -17,7 +17,7 @@ import Signup from '../pages/Signup';
 import Signout from '../pages/Signout';
 import DevTeam from '../pages/DevTeam';
 import EventsPage from '../pages/EventsPage';
-
+import Careers from '../pages/Careers';
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 class App extends React.Component {
@@ -32,6 +32,7 @@ class App extends React.Component {
             <Route path="/signup" component={Signup}/>
             <Route path="/signout" component={Signout}/>
             <Route path="/dev-team" component={DevTeam}/>
+            <Route path="/careers" component={Careers}/>
             <ProtectedRoute path="/event" component={EventsPage}/>
             <ProtectedRoute path="/list" component={ListStuff}/>
             <ProtectedRoute path="/add" component={AddStuff}/>
