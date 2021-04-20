ecdc5921eab3be9ead2b17bc2e17cd350e128d14
Ujjwal Gautam <ugautam@hawaii.edu>
2021-04-20 13:52:37 -1000

    WIP-3

--- app/imports/ui/pages/UserProfile.jsx
+++ app/imports/ui/pages/UserProfile.jsx
@@ -1,12 +1,10 @@
 // eslint-disable-next-line max-classes-per-file
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Grid, Image, Icon, Button, Header, Divider, Card, Message } from 'semantic-ui-react';
+import { Grid, Image, Icon, Button, Label, Header, Divider, Card, Message, Rating } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { _ } from 'meteor/underscore';
-// import { Stuffs } from '../../Stuff/Stuff';
-// import { Stuffs } from '../../api/stuff/Stuff';
 import { Profiles } from '../../Profiles/Profiles';
 /** A simple static component to render some text for the UserProfile page. */
 class UserProfile extends React.Component {
@@ -22,29 +20,32 @@ class UserDisplay extends React.Component {
   render() {
     const style = { marginTop: '20px' };
     const leftGrid = { marginLeft: '20px' };
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
     // eslint-disable-next-line react/prop-types
-    const { firstName, lastName, owner, bio, classes, reviews } = this.props.stuff;
+    const { firstName, lastName, imageLink, owner, bio, classes, reviews, city, state } = this.props.stuff;
+    console.log(imageLink);
     return (
-      <div>
+      <div className="userHP">
         <Grid id='UserProfile-page' verticalAlign='middle' textAlign='center'>
 
           <Grid.Column width={4}>
-            <Image size='medium' src="/images/logo.png"/>
+            <Rating maxRating={5} defaultRating={4} icon='star' size='massive' />
           </Grid.Column>
 
-          <Grid.Column width={8}>
+          <Grid.Column width={8} floated='left'>
             {/* eslint-disable-next-line no-undef */}
             <Header as='h1' color='yellow'> {firstName} {lastName}, Welcome to Rate My Classes</Header>
-            <p><b>Created by students for you!</b></p>
+            <h2><b>Created by students for you!</b></h2>
           </Grid.Column>
         </Grid>
         <div className="UserProfile-background"/>
-        <Grid centered stackable columns={6} style={style}>
+        <Grid centered stackable columns={3} style={style}>
           <Grid.Column textAlign='center' floated="left" width={5} style={leftGrid}>
-            <Card>
+            <Card fluid>
               {/* eslint-disable-next-line max-len */}
               <Image
-                src='https://www.businessinsider.in/photo/77825377/Lionel-Messi-leaving-Barcelona-would-be-the-biggest-and-most-shocking-transfer-in-football-history-but-it-would-make-perfect-sense-for-everyone.jpg?imgsize=330710'
+                src={imageLink}
                 wrapped circular ui={false}/>
               <Card.Content>
                 <Card.Header color='red'>{firstName} {lastName}</Card.Header>
@@ -58,36 +59,25 @@ class UserDisplay extends React.Component {
               <Card.Content extra>
                 <a>
                   <Icon name='location arrow'/>
-                    Honolulu
+                  {`${city}, ${state}`}
                 </a>
               </Card.Content>
             </Card>
           </Grid.Column>
           <Grid.Column textAlign='center' width={5} floated='left'>
-            <Header as='h1' color="blue">
+            <Header as='h1' color="brown">
              Classes Taken <Icon name="student" size="huge"/>
             </Header>
             {/* eslint-disable-next-line react/prop-types,react/jsx-key */}
-            {classes.map((item) => <div key={item}><Button fluid key={item} primary content={item} /> <br /></div>)}
-            <Divider horizontal fitted inverted>---</Divider>
+            {classes.map((item) => <div key={item}><Button fluid key={item} size='big' content={item} style={{ color: `#${randomColor}` }} /> <br /></div>)}
+            <Divider horizontal fitted>-----------------------------</Divider>
 
-            <Header as='h1'>Pending Reviews</Header>
-            {/* eslint-disable-next-line array-callback-return,react/prop-types */}
-            {/* {_.each(reviews, (item) => <div><Button>{item}haha</Button>hmm </div>)} */}
-            {/* eslint-disable-next-line react/prop-types,react/jsx-key,react/no-unescaped-entities */}
-            {/* <div>{_.mapObject(reviews, (val, key) => <div><Button>{key}</Button></div>)})}</div> */}
+            <Header as='h1' color='brown'>Pending Reviews</Header>
             {/* eslint-disable-next-line react/jsx-no-undef */}
             <div>
-              {/* {_.mapObject(reviews, function (val, key) { return (<Button>{key}</Button>); })} */}
-              {/* {_.map(_.values(reviews), val => <Message>{val}</Message>) } */}
-              {_.map(_.keys(reviews), (key) => <div key="key" ><span><Button color='blue'>{key}</Button>
+              {_.map(_.keys(reviews), (key) => <Message key={key}><span><Label pointing='below' size='big' style={{ color: `#${randomColor}` }}>{key}</Label>
                 <Message size='large' color='yellow'>{reviews[key]}</Message></span>
-              </div>)}
-              {/* eslint-disable-next-line no-unused-vars */}
-              {/* {_.map(_.keys(reviews), (item) => <div><Button key={item} inverted>{item}</Button> */}
-              {/*  /!* eslint-disable-next-line react/prop-types *!/ */}
-              {/*  /!*<Message key={key}>{reviews.key}</Message>*!/ */}
-              {/* </div>)} */}
+              <br /></Message>)}
             </div>
           </Grid.Column>
           <Grid.Column textAlign='center'>
@@ -104,42 +94,37 @@ class UserDisplay extends React.Component {
   }
 }
 
-// class Usersub extends React.Component {
-//   render() {
-//     // eslint-disable-next-line react/prop-types
-//     const obj = this.props.reviews;
-//     console.log(obj);
-//     return (
-//         // eslint-disable-next-line no-unused-vars
-//       <div>{_.mapObject(obj, (key, value) => <Button>{key}</Button>)}</div>
-//     );
-//   }
-// }
-
 // Require an array of Stuff documents in the props.
 UserProfile.propTypes = {
   // profiles: PropTypes.shape({
-  //   name: PropTypes.string,
-  //   quantity: PropTypes.number,
-  //   condition: PropTypes.string,
-  //   _id: PropTypes.string,
+  //   firstName: PropTypes.string,
+  //   lastName: PropTypes.string,
   //   owner: PropTypes.string,
+  //   _id: PropTypes.string,
+  //   bio: PropTypes.string,
+  //   classes: PropTypes.array,
+  //   reviews: PropTypes.object,
   // }).isRequired,
   profiles: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
-// UserDisplay.propTypes = {
-//   // profiles: PropTypes.shape({
-//   //   name: PropTypes.string,
-//   //   quantity: PropTypes.number,
-//   //   condition: PropTypes.string,
-//   //   _id: PropTypes.string,
-//   //   owner: PropTypes.string,
-//   // }).isRequired,
-//   stuff: PropTypes.Object,
-//   // key: PropTypes.String,
-// };
+UserDisplay.propTypes = {
+  stuff: PropTypes.shape({
+    firstName: PropTypes.string,
+    lastName: PropTypes.string,
+    imageLink: PropTypes.string,
+    state: PropTypes.string,
+    city: PropTypes.string,
+    owner: PropTypes.string,
+    _id: PropTypes.string,
+    bio: PropTypes.string,
+    classes: PropTypes.array,
+    reviews: PropTypes.object,
+  }).isRequired,
+  // stuff: PropTypes.Object,
+  // key: PropTypes.String,
+};
 
 // withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
 export default withTracker(() => {
