a6b8edad4f93157bf4a89fa399167f061f7f085c
Mr. Young <masyoung@hawaii.edu>
2021-05-01 19:42:37 -1000

    When the admin approves a class review, the Class List Collection is updated.

--- app/imports/ui/pages/AdminProfile.jsx
+++ app/imports/ui/pages/AdminProfile.jsx
@@ -10,6 +10,8 @@ import { Profiles } from '../../Profiles/Profiles';
 import AdminInfo from '../components/AdminInfo';
 import { ClassReviews } from '../../api/classReview/ClassReview';
 import { ProfessorReviews } from '../../api/professorReview/ProfessorReview';
+import { ClassList } from '../../api/classList/ClassList';
+
 /** A simple static component to render some text for the AdminProfile page. */
 class AdminProfile extends React.Component {
   render() {
@@ -24,6 +26,58 @@ class AdminProfile extends React.Component {
   }
 }
 class AdminDisplay extends React.Component {
+
+  // updates the total review count for the Class List Publication
+  updateTotalClassReviewsCount(className) {
+    const record = ClassList.collection.findOne({ class: className });
+    const totalRating = record.totalRatings + 1;
+    ClassList.collection.update(record._id, { $set: { totalRatings: totalRating } });
+  }
+
+  // update the average rating for the Class List Publication
+  updateAvgRating(className, stars) {
+    const record = ClassList.collection.findOne({ class: className });
+    const totalRatings = record.totalRatings;
+    const avgRating = (record.avgRating + stars) / totalRatings;
+    ClassList.collection.update(record._id, { $set: { avgRating: avgRating } });
+  }
+
+  // update the number of stars for the Class List Publication
+  updateRatingsCount(className, stars) {
+    const record = ClassList.collection.findOne({ class: className });
+    const ratings = {
+      1: record['1star'],
+      2: record['2star'],
+      3: record['3star'],
+      4: record['4star'],
+      5: record['5star'],
+    };
+
+    switch (stars) {
+    case 1:
+      ClassList.collection.update(record._id, { $set: { '1star': ratings[1] + 1 } });
+      this.updateAvgRating(className, stars);
+      break;
+    case 2:
+      ClassList.collection.update(record._id, { $set: { '2star': ratings[2] + 1 } });
+      this.updateAvgRating(className, stars);
+      break;
+    case 3:
+      ClassList.collection.update(record._id, { $set: { '3star': ratings[3] + 1 } });
+      this.updateAvgRating(className, stars);
+      break;
+    case 4:
+      ClassList.collection.update(record._id, { $set: { '4star': ratings[4] + 1 } });
+      this.updateAvgRating(className, stars);
+      break;
+    case 5:
+      ClassList.collection.update(record._id, { $set: { '5star': ratings[5] + 1 } });
+      this.updateAvgRating(className, stars);
+      break;
+    default:
+    }
+  }
+
   // approve the class review
   approvedClassReview(data) {
     const { createdAt, className, rating, review, owner, _id } = data;
@@ -35,6 +89,8 @@ class AdminDisplay extends React.Component {
         icon: 'success',
         button: 'Go',
       })));
+    this.updateTotalClassReviewsCount(className);
+    this.updateRatingsCount(className, rating);
   }
 
   // delete the class review when the admin clicks on the red button
@@ -170,6 +226,7 @@ class AdminDisplay extends React.Component {
 // Specify types of documents in the props.
 AdminProfile.propTypes = {
   profiles: PropTypes.array.isRequired,
+  classList: PropTypes.array.isRequired,
   classReview: PropTypes.array.isRequired,
   professorReview: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
@@ -195,16 +252,19 @@ AdminDisplay.propTypes = {
 export default withTracker(() => {
   // Get access to collections.
   const subscription = Meteor.subscribe(Profiles.adminPublicationName);
+  const classListSub = Meteor.subscribe(ClassList.generalPublicationName);
   const classReviewsSub = Meteor.subscribe(ClassReviews.adminPublicationName);
   const professorReviewsSub = Meteor.subscribe(ProfessorReviews.adminPublicationName);
   // Get the data from collections
   const profiles = Profiles.collection.find({}).fetch();
+  const classList = ClassList.collection.find({}).fetch();
   const classReview = ClassReviews.collection.find({}).fetch();
   const professorReview = ProfessorReviews.collection.find({}).fetch();
   return {
     profiles,
+    classList,
     classReview,
     professorReview,
-    ready: subscription.ready() && classReviewsSub.ready() && professorReviewsSub.ready(),
+    ready: subscription.ready() && classListSub && classReviewsSub.ready() && professorReviewsSub.ready(),
   };
 })(AdminProfile);
