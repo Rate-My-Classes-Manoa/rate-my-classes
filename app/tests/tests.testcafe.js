1d5935a68585b7bc38a38f7c5f57352773a5cd15
Ujjwal Gautam <ugautam@hawaii.edu>
2021-05-08 13:57:33 -1000

    Update tests.testcafe.js

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -77,7 +77,7 @@ test('Test Profile edit', async (testController) => {
   await signoutPage.isDisplayed(testController);
 });
 
-test.only('Test Events page', async (testController) => {
+test('Test Events page', async (testController) => {
   await navBar.gotoSigninPage(testController);
   await signinPage.signin(testController, credentials.username, credentials.password);
   await navBar.isLoggedIn(testController, credentials.username);
